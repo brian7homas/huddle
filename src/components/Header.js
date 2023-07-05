4bd0093a66883485fb42879dffa943a0f9c56e0d
Brian Thomas <btdeveloper125@gmail.com>
2023-07-05 09:22:52 -0600

    refactor(Header): ADD LOGO
    added logo svg to header
    add base styles for 1440px

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react"
+import React from "react"
+import Logo from '../../assets/logo.svg'
 
 function Header() {
   return (
-    <header data-depth="0.2">
-      <p>Layer 1</p>
+    <header data-depth="0.2" className="header">
+      <section>
+        <Logo className="header_logo"/>
+      </section>
     </header>
   )
 }
