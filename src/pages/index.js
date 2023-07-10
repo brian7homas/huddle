63930726b864247809679d4d220ba8060698290c
Brian Thomas <btdeveloper125@gmail.com>
2023-07-10 14:29:09 -0600

    refactor(Home): BACKGROUND IMG
    added svg to static folder to beaccessed through css

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,14 +15,7 @@ export default function Home() {
   }, [])
 
   return (
-    <main id="scene" style={{
-      background: `url('fullscreen.svg') no-repeat, 
-      linear-gradient(217deg,#e882e8,rgba(55, 0, 100, .15) 70.71%),
-      linear-gradient(127deg, hsla(257, 40%, 49%, 1), hsla(257, 40%, 49%, .5) 70.71%),
-      linear-gradient(336deg, hsla(257, 40%, 49%, 1), hsla(257, 40%, 49%, 1) 70.71%)`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'left center'
-    }}>
+    <main id="scene">
       <MainContainer>
         <Header />
         <ColumnContainer />
