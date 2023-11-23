af7ae670e9a09ff85d17cedcccab2a600b2e0ab8
Brian Thomas <btdeveloper125@gmail.com>
2023-11-23 12:26:37 -0700

    refactor(Html): HTML CORRECTIONS

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,9 +2,9 @@ import React from "react"
 
 function MainContainer({children}) {
   return (
-    <section className="main-container">
+    <div className="main-container">
       {children}
-    </section>
+    </div>
   )
 }
 
