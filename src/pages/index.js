6b406b3ca2a81738dd84ce9367de9229a16e91a8
Brian Thomas <btdeveloper125@gmail.com>
2023-11-23 12:31:55 -0700

    feat(SquaresRender): RENDER PARALLAX SQUARES
    component used to render parallax squares
    entry point for parallx-js
    handleWindowSize - sets innerWidth and returns loadParallax based on breakpoint

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,15 @@ import Parallax from "parallax-js"
 import Header from "../components/Header"
 import MainContainer from "../components/MainContainer"
 import ColumnContainer from "../components/ColumnContainer"
+import SquareRender from "../components/squaresRender"
 import { Seo }  from "../components/Seo"
 import UseIsClient from "../components/UseIsClient"
+const Home = () => {
   const { isClient } = UseIsClient()
   if( !isClient ) return null
   return (
     <main id="scene">
+      <SquareRender />
       <MainContainer>
         <Header />
         <ColumnContainer />
@@ -17,4 +20,5 @@ import UseIsClient from "../components/UseIsClient"
     </main>
   )
 }
+export default Home
     <Seo
