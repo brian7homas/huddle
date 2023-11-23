1c83acce401cb0cbafda62ecbcfe08d5dd0dcb7e
Brian Thomas <btdeveloper125@gmail.com>
2023-11-23 12:17:07 -0700

    refactor(Squares) MOVING RECTANGLE/SQUARES
    parrallaxed squares

---
+++ src/components/Squares.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+function Squares() {
+  return (
+    <svg id="squares-container" xmlns="http://www.w3.org/2000/svg" width="1260.202" height="740.954" viewBox="0 0 1560.202 740.954">
+      <defs>
+        <linearGradient id="linear-gradient" x1="0.217" y1="0.05" x2="1.459" y2="1.456" gradientUnits="objectBoundingBox">
+          <stop offset="0" stop-color="#ff52c1" />
+          <stop offset="1" stop-color="#9952ff" />
+        </linearGradient>
+        <linearGradient id="linear-gradient-6" x1="0.064" y1="-0.152" x2="0.918" y2="1.057" />
+      </defs>
+      <g data-depth=".9">
+        <rect id="Rectangle_61" data-name="Rectangle 61" width="32" height="32" rx="3" transform="translate(995.03 41.003) rotate(45)" opacity="0.558" fill="url(#linear-gradient)" />
+      </g>
+      <g data-depth="3.05" data-name="Group 33" transform="translate(-119.398 -41.003)">
+        <circle id="Ellipse_25" data-name="Ellipse 25" cx="49" cy="49" r="49" transform="translate(715.4 514)" fill="transparent" stroke="#707070" stroke-opacity=".4" stroke-width="17" />
+      </g>
+      <g data-depth=".3">
+        <rect id="Rectangle_62" data-name="Rectangle 62" width="38" height="38" rx="3" transform="translate(1168.271 682.006) rotate(45)" opacity="0.558" fill="url(#linear-gradient)" />
+      </g>
+      <g data-depth=".99">
+        <rect id="Rectangle_64" data-name="Rectangle 64" width="56" height="56" rx="3" transform="translate(1340.002 64.004) rotate(45)" opacity="0.503" fill="url(#linear-gradient)" />
+      </g>
+      <g data-depth=".69">
+        <rect data-depth=".5" id="Rectangle_60" data-name="Rectangle 60" width="32" height="32" rx="3" transform="translate(764.029 164.002) rotate(45)" opacity="0.558" fill="url(#linear-gradient)" />
+      </g>
+      <g data-depth=".4">
+        <rect id="Rectangle_63" data-name="Rectangle 63" width="17" height="17" rx="1" transform="translate(1297.422 594.006) rotate(45)" opacity="0.387" fill="url(#linear-gradient)" />
+      </g>
+      <g data-depth=".2" data-name="Group 33" transform="translate(-119.398 -41.003)">
+        <path id="Path_12" data-name="Path 12" d="M91.477,739.477v-16.5a5,5,0,0,1,10,0v16.5h16.5a5,5,0,0,1,0,10h-16.5v16.5a5,5,0,1,1-10,0v-16.5h-16.5a5,5,0,0,1,0-10Z" transform="translate(615.082 149.833) rotate(45)" fill-rule="evenodd" opacity="0.211" fill="#ff52c1" />
+      </g>
+    </svg>
+  )
+}
+export default Squares
